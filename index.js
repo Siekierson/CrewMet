b993f2c4e91668b064c45c044d0696225e89c154
Siekierson <kisiel123.kisiel@gmail.com>
2020-07-31 01:10:36 +0200

    .

--- index.js
+++ index.js
@@ -24,4 +24,13 @@ app.use('/users',usersRouter);
 app.use('/meetings',meetingsRouter);
 app.use('/messages',messagesRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 app.listen(port,()=>console.log(`Server is listening at http://localhost:${port}`));
