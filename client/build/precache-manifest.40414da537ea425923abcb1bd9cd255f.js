963c8d338f9cf697afa68fb37e0fbce4b2994bb8
Siekierson <kisiel123.kisiel@gmail.com>
2020-07-31 12:15:34 +0200

    build

---
+++ client/build/precache-manifest.40414da537ea425923abcb1bd9cd255f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50af5c696636e5c66c4bec11d2de6af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4eeb3b29e0952e4e4a82",
+    "url": "/static/css/main.62d43837.chunk.css"
+  },
+  {
+    "revision": "2202b3eaf779ce8d8f5b",
+    "url": "/static/js/2.2e6e6bed.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.2e6e6bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eeb3b29e0952e4e4a82",
+    "url": "/static/js/main.a4b68983.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "cbc78d9e2fe995724e748c3ebda1fda8",
+    "url": "/static/media/friends.cbc78d9e.mp4"
+  }
+]);
